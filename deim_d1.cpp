25d9eafd3071576376a70c434bee059d10768f0e
CIRQUE1015 <cirque1015@gmail.com>
2012-01-18 14:19:33 +0900

    プログラム追加

---
+++ deim_d1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+PrintScale(int L, int R);
+
+int main(){
+
+  FILE *fp;
+
+  fp=fopen("test.dat","r");
+  
+  while(fp==!NULL){
+    fget
+  }  
+
+  fclose(fp);
+  return 0;
+}
+
+PrintScale(int L, int R){
+
+  int i;
+  for(i=L; i<=R; i++){
+    printf("%d ",P[i]);
+  }
+  printf("\n");
+
+}
